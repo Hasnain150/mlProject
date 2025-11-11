d805d4f950c8f7636aeedf0b8c1f080721354806
sunny <hasnainshahzad1455@gmail.com>
2025-11-11 21:43:37 +0500

    Minor changes

--- src/exception.py
+++ src/exception.py
@@ -9,7 +9,6 @@ def error_message_details(error, error_details:sys):
         file_name,exc_tb.tb_lineno, str(error)) 
     return error_message
 
-
 class CustomException(Exception) : 
     def __init__(self,error_message, error_detial:sys) : 
         super().__init__(error_message)
