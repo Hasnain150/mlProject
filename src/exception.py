2ef0bf72a8e63b05a5bf29c839cce5cfb9be4fd3
sunny <hasnainshahzad1455@gmail.com>
2025-11-20 13:48:36 +0500

    edited the EDA file

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,9 @@
 import sys
 import logging
+from typing import String
 from src.logger import logging
 
-def error_message_details(error, error_details:sys): 
+def error_message_details(error, error_details:sys)-> String: 
     _,_,exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] and the message is [{2}]".format(
